6c0a2cc5632e3ed67c86e5175af116e5f16ea886
Minato1123 <slamdunk0522@gmail.com>
2023-03-27 18:39:27 +0800

    feat: store/user add userId

--- stores/user.ts
+++ stores/user.ts
@@ -2,19 +2,23 @@ export const useUserStore = defineStore(
   'user',
   () => {
     const userToken = ref('')
+    const userId = ref<number | null>()
 
     const isLoggedin = computed<boolean>(() => userToken.value !== '')
 
-    function userLogin(token: string) {
-      userToken.value = token
+    function userLogin(_token: string, _userId: number) {
+      userToken.value = _token
+      userId.value = _userId
     }
 
     function userLogout() {
       userToken.value = ''
+      userId.value = null
     }
 
     return {
       userToken,
+      userId,
       isLoggedin,
       userLogin,
       userLogout,
