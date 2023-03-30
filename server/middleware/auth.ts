c8d349468783b88dcd880a116761066d2a53f1c5
Minato1123 <slamdunk0522@gmail.com>
2023-03-30 14:49:06 +0800

    fix: auth can't get tokenMap

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,8 +1,9 @@
-import { tokenMap } from '../tokenMap'
+import { getData } from '../utils/'
 
-export default defineEventHandler((event) => {
+export default defineEventHandler(async (event) => {
+  const db = await getData()
   // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
   const theToken = event.node.req.headers['access-token'] as string
-  const userId = tokenMap.get(theToken)
+  const userId = db.token_map.find(t => t.token === theToken)?.userId
   event.context.auth = { userId }
 })
